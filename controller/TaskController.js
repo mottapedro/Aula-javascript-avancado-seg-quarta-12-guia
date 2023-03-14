1ffa1a7c829dfbed53f82cf4cce299fcaec3d98e
mottapedro <mottapedro01@gmail.com>
2023-03-14 00:00:19 -0300

    commit : getById, updateOneTask

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,11 +3,11 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {//modulo criado 
     try {
         const taskList = await Task.find();
-        res.render("index", { taskList });
+        return res.render("index", { taskList, task: null });
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
-}
+};
 
 const createTask = async (req, res) => {
     const task = req.body;
@@ -15,6 +15,8 @@ const createTask = async (req, res) => {
     if (!task.task) {
         return res.redirect("/");
     }
+
+
 
     try {
         await Task.create(task)
@@ -24,7 +26,31 @@ const createTask = async (req, res) => {
     }
 };
 
+const getById = async (req, res) => {
+    //enviar o id como parametro da rota
+    try {
+        const task = await Task.findOne({ _id: req.params.id });
+        const taskList = await Task.find();
+        res.render("index", { task, taskList });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const updateOneTask = async (res, req) => {
+    try {
+
+        const task = req.body;
+        await Task.updateOne({ _id: req.params.id }, task);
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+}
+
 module.exports = {
     getAllTasks,
-    createTask
-}
+    createTask,
+    getById,
+    updateOneTask
+};
